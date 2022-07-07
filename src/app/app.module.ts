7f63dbe91635428151c097e6d6db3666870f3aba
AndressaBiagi <andressajbiagi@gmail.com>
2022-07-07 11:27:57 -0300

    upload base project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { StoreAppComponent } from './components/store-app/store-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    FiltersComponent,
+    StoreAppComponent
   ],
   imports: [
     BrowserModule,
